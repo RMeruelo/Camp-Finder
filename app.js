fcca832b706b0cce586a82ec441ea7eb6f3956c7
Raul Meruelo <raulameruelo@hotmail.com>
2022-08-23 12:55:08 -0400

    attempted fix

--- app.js
+++ app.js
@@ -85,7 +85,9 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 
-app.use(express.static(path.join(__dirname,'public')))
+//app.use(express.static(path.join(__dirname,'public')))
+
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(mongoSanitize())
